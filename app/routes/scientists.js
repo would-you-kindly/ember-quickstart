481d8aba1f5282ac19d5cb29e7445ae53dda13c6
ashchelkunov <ashchelkunov@ICS-KK16811>
2020-11-19 13:40:16 +0500

    Первый пример приложения Ember

---
+++ app/routes/scientists.js
@@ -0,0 +1,8 @@
+/* jshint esversion: 6 */
+import Route from '@ember/routing/route';
+
+export default class ScientistsRoute extends Route {
+    model() {
+        return ['Marie Curie', 'Mae Jemison', 'Albert Hofmann'];
+      }
+}
